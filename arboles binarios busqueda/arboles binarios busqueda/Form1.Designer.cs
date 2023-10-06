2949c01b383d2cce3f1d3fb8db0066b191bef54d
AlejandroVelezGuillermo <125221168+AlejandroVelezGuillermo@users.noreply.github.com>
2023-10-05 18:48:43 -0600

    Add files via upload

---
+++ arboles binarios busqueda/arboles binarios busqueda/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace arboles_binarios_busqueda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(144, 117);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 0;
+            this.btnInsert.Text = "button1";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(253, 117);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "button1";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(362, 117);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "button1";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(101, 187);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(100, 22);
+            this.txtValue.TabIndex = 3;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(101, 234);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(44, 16);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnInsert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ arboles binarios busqueda/arboles binarios busqueda/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arboles_binarios_busqueda
+{
+    public partial class Form1 : Form
+    {
+        BinaryTree tree = new BinaryTree();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtValue.Text, out int value))
+            {
+                tree.Insert(value);
+                lblResult.Text = $"Valor {value} insertado en el árbol.";
+            }
+            else
+            {
+                lblResult.Text = "Ingresa un valor válido.";
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtValue.Text, out int value))
+            {
+                if (tree.Delete(value))
+                {
+                    lblResult.Text = $"Valor {value} eliminado del árbol.";
+                }
+                else
+                {
+                    lblResult.Text = $"El valor {value} no se encontró en el árbol.";
+                }
+            }
+            else
+            {
+                lblResult.Text = "Ingresa un valor válido.";
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtValue.Text, out int value))
+            {
+                if (tree.Search(value))
+                {
+                    lblResult.Text = $"El valor {value} se encuentra en el árbol.";
+                }
+                else
+                {
+                    lblResult.Text = $"El valor {value} no se encontró en el árbol.";
+                }
+            }
+            else
+            {
+                lblResult.Text = "Ingresa un valor válido.";
+            }
+        }
+    }
+
+    internal class BinaryTree
+    {
+        internal bool Delete(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Insert(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool Search(int value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
